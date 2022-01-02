7c76c2c62f450661391cd4ed921c2378a5717d72
ppammer7 <94915490+ppammer7@users.noreply.github.com>
2022-01-02 18:41:42 +0100

    Lohnrechner
    Lohnrechner

---
+++ Lohnrechner.py
@@ -0,0 +1,10 @@
+stundenlohn = input("Bitte geben Sie ihren Stundenlohn ein...")
+tag = 8 * int(stundenlohn)
+monat = 20 * int(tag)
+jahr = 12 * int(monat)
+
+print("Du verdienst " + str(tag) + "€ pro Tag.")
+print("Du verdienst " + str(monat) + "€ pro Monat.")
+print("Du verdienst "+ str(jahr) + "€ pro Jahr.")
+
+input("")
